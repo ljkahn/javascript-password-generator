c8e050a90156dce34ba2c99c3de811c19a1bff91
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-08-26 18:58:39 -0600

    added variable to store password array

--- script.js
+++ script.js
@@ -12,11 +12,26 @@ var upperCaseChar = ["A", "B", "C"];
 var generateBtn = document.querySelector("#generate"); // storing a reference to an element
 
 
+//this functions purpose is getting a random element from an array
+
+function random(arr) {
+  //using Math.floor and Math.random --> pick one arrayy 
+// arr = ["+", "!", "#"]
+
+var randomIndex = Math.floor(Math.random() * arr.length);
+
+
+var randomChar = arr[randomIndex];
+
+} 
 
 
 function generatePassword() {
   //captures user length for password
  var userCharsLength = prompt("How many characters would you like your password to contain?");
+ if (userCharsLength< 8 || userCharsLength >128) {
+  prompt("Try Again!");
+ }
  
  var specialCharacters= confirm("Click OK to confirm including special characters.");
 
@@ -37,9 +52,48 @@ var userOptions = {
   willHaveUppChars: upperCaseCharacters
 
 
-}
 
 }
+
+var password = [];
+if (userOptions.willHaveSpecChars) {
+  //lets randomly grab a member from the special characters array
+  //push into the result array
+  random(specialChars);
+
+
+}
+
+if (userOptions.willHaveNumChars) {
+  //lets randomly grab a member from the numerical characters array
+  //push into the result array
+  random(numericChars);
+
+
+}
+if (userOptions.willHaveLowChars) {
+  //lets randomly grab a member from the lowercase characters array
+  //push into the result array
+  random(lowerCaseChar);
+
+}
+if (userOptions.willHaveUppChars) {
+  //lets randomly grab a member from the uppercase characters array
+  //push into the result array
+random(upperCaseChar);
+  
+}
+
+
+
+}
+
+
+//eventually this array will store the random generated password. i will use push method to add to this array and this is what i will return to the user.
+
+
+
+
 
 
 
@@ -48,10 +102,12 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-} //storing a reference to the text area element
+} 
+//storing a reference to the text area element
 
 
-// passwordText.value = password;
+
+
 
 // // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
